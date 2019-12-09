4703febaa3574733154d2c9415a49b33f0612cbd
Simon <simonschllng@users.noreply.github.com>
2019-12-09 16:30:10 +0100

    Select action on invocation
    `rMsync -[operation]` where [operation] in b=backup, d=download, u=upload

--- rM-sync.sh
+++ rM-sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Sync script for the reMarkable reader
-# Version: 0.1
+# Version: 0.2
 # Author: Simon Schilling
 # Licence: MIT
 
@@ -17,6 +17,7 @@ BACKUPDIR="$MAINDIR/backup/"             # rotating backups of all rM contents
 UPLOADDIR="$MAINDIR/upload/"             # all files here will be sent to rM
 OUTPUTDIR="$MAINDIR/files/"              # PDFs of everything on the rM
 LOG="sync.log"                          # Log file name in $MAINDIR
+BACKUPLIST="files.json"
 
 # Behaviour
 NOTIFICATION="/usr/bin/notify-send"     # Notification script
@@ -27,9 +28,7 @@ echo $'\n' >> $LOG
 date >> $LOG
 
 
-if [ "$RMUSER" ] && [ "$SSHPORT" ]; then
-  S="ssh -p $SSHPORT -l $RMUSER";
-fi
+S="ssh -p $SSHPORT -l $RMUSER";
 
 # check for rM
 $S $RMIP -q exit
@@ -38,49 +37,82 @@ if [ $? == "0" ]; then
 
   TODAY=$(date +%y%m%d)
 
-  # Backup files
-  echo "BEGIN BACKUP" >> $LOG
-  mkdir -p "$BACKUPDIR$TODAY"
-  echo "scp \"$RMUSER@$RMIP:$RMDIR\" $BACKUPDIR$TODAY"  >> $LOG
-  scp -r "$RMUSER@$RMIP:\"$RMDIR\"*" "$BACKUPDIR"$TODAY >> $LOG 2>&1
-  if [ $? -ne 0 ]; then
-    ERRORREASON=$ERRORREASON$'\n scp command failed'
-    ERROR=1
-  fi
-  echo "BACKUP END" >> $LOG
 
-
-
-  # Download files
-  echo "BEGIN DOWNLOAD" >> $LOG
-  mkdir -p "$OUTPUTDIR"
-  ls -1 "$BACKUPDIR$TODAY" | sed -e 's/\..*//g' | awk '!a[$0]++' > "$OUTPUTDIR/index"
-  echo "Downloading" $(wc -l "$OUTPUTDIR/index") "files." >> $LOG
-  # http://$RMIP/download/$FILEUID/placeholder
-  while read -r line
+  while getopts bdu opt
   do
-      FILEUID="$line"
-      curl -s -O -J -L "http://$RMIP/download/$FILEUID/placeholder"
-      if [ $? -ne 0 ]; then
-        ERRORREASON=$ERRORREASON$'\n Download failed'
-        ERROR=1
-      fi
-  done < "$OUTPUTDIR/index"
-  echo "DOWNLOAD END" >> $LOG
+     case $opt in
+       b) 
+
+          # Backup files
+          echo "BEGIN BACKUP" | tee -a $LOG
+          mkdir -p "$BACKUPDIR$TODAY"
+          echo "scp \"$RMUSER@$RMIP:$RMDIR\" $BACKUPDIR$TODAY"  >> $LOG
+          scp -r "$RMUSER@$RMIP:\"$RMDIR\"*" "$BACKUPDIR"$TODAY >> $LOG 2>&1
+          if [ $? -ne 0 ]; then
+            ERRORREASON=$ERRORREASON$'\n scp command failed'
+            ERROR=1
+          fi
+          echo "[" > "$BACKUPDIR$TODAY$BACKUPLIST"
+          find "$BACKUPDIR$TODAY" -name *.metadata -type f -exec sed -s '$a,' {} + | sed '$d' >> "$BACKUPDIR$TODAY$BACKUPLIST"
+          echo "]" >> "$BACKUPDIR$TODAY$BACKUPLIST"
+          echo "BACKUP END" | tee -a $LOG
+          ;;
+
+        d)
+          # Download files
+          echo "BEGIN DOWNLOAD" | tee -a $LOG
+          mkdir -p "$OUTPUTDIR"
+          ls -1 "$BACKUPDIR$TODAY" | sed -e 's/\..*//g' | awk '!a[$0]++' > "$OUTPUTDIR/index"
+          
+          echo "[" > "$OUTPUTDIR/index.json";
+          for file in "$BACKUPDIR$TODAY"/*.metadata;
+          do
+              [ -e "$file" ] || continue
+              echo "{" >> "$OUTPUTDIR/index.json";
+              echo "    \"id\": \"$(basename "$file" .metadata)\"," >> "$OUTPUTDIR/index.json";
+              tail --lines=+2 "$file" >> "$OUTPUTDIR/index.json";
+              echo "," >> "$OUTPUTDIR/index.json";
+          done
+          truncate -s-2 "$OUTPUTDIR/index.json"; #Remove last koma
+          echo "]" >> "$OUTPUTDIR/index.json";
 
 
-  # Upload files
-  echo "BEGIN UPLOAD" >> $LOG
-  # TODO
-  if [ $? -ne 0 ]; then
-    ERRORREASON=$ERRORREASON$'\n Upload failed'
-    ERROR=1
-  fi
-  echo "UPLOAD END" >> $LOG
+          echo "Downloading" $(wc -l < "$OUTPUTDIR/index") "files." | tee -a $LOG
+          # http://$RMIP/download/$FILEUID/placeholder
+          while read -r line
+          do
+              FILEUID="$line"
+              #curl -s -O -J -L "http://$RMIP/download/$FILEUID/placeholder"
+              if [ $? -ne 0 ]; then
+                ERRORREASON=$ERRORREASON$'\n Download failed'
+                ERROR=1
+              fi
+          done < "$OUTPUTDIR/index"
+        echo "DOWNLOAD END" | tee -a $LOG
+        ;;
 
+      u)
+        # Upload files
+        echo "BEGIN UPLOAD" | tee -a $LOG
+        for file in "$UPLOADDIR"/*;
+        do
+            [ -e "$file" ] || continue
+            echo -n $(basename "$file") ": "
+            curl --form "file=@\"$file\"" http://10.11.99.1/upload
+            echo "."
+            if [ 0 -eq $? ]; then rm "$file"; fi;
+        done
+        if [ $? -ne 0 ]; then
+          ERRORREASON=$ERRORREASON$'\n Upload failed'
+          ERROR=1
+        fi
+        echo "UPLOAD END" | tee -a $LOG
+        ;;
+    esac
+  done
 
 else
-  echo "reMarkable not connected" >> $LOG
+  echo "reMarkable not connected" | tee -a $LOG
   ERRORREASON=$ERRORREASON$'\n reMarkable not connected'
   ERROR=1
 fi
